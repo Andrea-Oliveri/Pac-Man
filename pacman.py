b08556576f314b0cd7479080e519590b5c796696
Andrea Oliveri <andrea_oliveri@outlook.com>
2023-12-21 08:30:22 +0100

    Fixed occasional issue with startup. See pyglet/pyglet#1019

--- pacman.py
+++ pacman.py
@@ -8,8 +8,15 @@ if getattr(sys, 'frozen', False):
     os.chdir(sys._MEIPASS)
 
 
+import pyglet
 
-import pyglet.app
+# This improves stability by preventing strange occasional crashes when creating main window.
+# It also appears to avoid other occasional issue where process takes long time to end after window closed.
+# See GitHub issue pyglet/pyglet#1019
+pyglet.options['shadow_window'] = False
+
+
+
 from src.window import Window
 
 window = Window()
