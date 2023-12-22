17e765314433f31d7443a357d477d50403466293
Andrea Oliveri <andrea_oliveri@outlook.com>
2023-12-22 16:29:48 +0100

    Added constants linked to level states

--- src/constants.py
+++ src/constants.py
@@ -343,6 +343,37 @@ def FRUIT_OF_LEVEL(level):
 
 
 # --------------------------------------------------------------------
+# Constants related to Level animations.
+# --------------------------------------------------------------------
+
+# Possible states for the level.
+LevelStates = IntEnum('LevelStates', ['FIRST_WELCOME', 'READY', 'PLAYING', 'PAUSED', 'DEATH', 'COMPLETED'])
+
+# Duration (in original game frames) of animations corresponding to different level states.
+LEVEL_FIRST_WELCOME_PERIOD_FRAMES             = 150
+LEVEL_READY_PERIOD_FRAMES                     = 120
+LEVEL_PAUSE_BEFORE_DEATH_PERIOD_FRAMES        = 60
+LEVEL_DEATH_PERIOD_FRAMES                     = sum(PACMAN_DEATH_ANIMATION_PERIOD_FRAMES) + 50
+LEVEL_PAUSE_BEFORE_COMPLETED_PERIOD_FRAMES    = 120
+LEVEL_COMPLETED_FLASH_ANIMATION_PERIOD_FRAMES = 12
+LEVEL_PAUSE_AFTER_EATING_PERIOD_FRAMES        = 60
+
+# Coordinates and color of 'PLAYER ONE' text.
+LEVEL_PLAYER_ONE_TEXT_COORDS = (-4.5 * MAZE_TILE_PX_SIZE, +4 * MAZE_TILE_PX_SIZE)
+LEVEL_PLAYER_ONE_TEXT_COLOR  = FontColors.CYAN
+
+# Coordinates and color of 'READY!' text.
+LEVEL_READY_TEXT_COORDS      = (-2.5 * MAZE_TILE_PX_SIZE, -2 * MAZE_TILE_PX_SIZE)
+LEVEL_READY_TEXT_COLOR       = FontColors.YELLOW
+
+# Number of white flashes for the level completed animation. Level end animation: white, blue, white, blue, white, blue, white, blue.
+LEVEL_COMPLETED_FLASH_NUM = 4
+
+# --------------------------------------------------------------------
+
+
+
+# --------------------------------------------------------------------
 # Constants related to Score.
 # --------------------------------------------------------------------
 
@@ -400,6 +431,7 @@ def FRIGHT_TIME_AND_FLASHES(level):
 
 
 # --------------------------------------------------------------------
+
 
 
 # --------------------------------------------------------------------
