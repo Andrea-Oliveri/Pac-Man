45be0e6d1f39d7feddc6186bca9edb8da67ad06e
Andrea-Oliveri <andrea_oliveri@outloock.com>
2025-08-09 17:18:07 +0200

    Used weakref.finalized to write high score to disk only once, when program closes

--- src/game_objects/score.py
+++ src/game_objects/score.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import weakref
+from dataclasses import dataclass
 
 from src.constants import (ScoreActions,
                            SCORE_POINTS_EAT_PELLET,
@@ -10,43 +12,58 @@ from src.constants import (ScoreActions,
                            HIGH_SCORE_FILE,
                            HIGH_SCORE_FILE_NUM_BYTES)
 
+
+# Dataclass needed just to hold the high-score value so we can pass it to weakref.finalizer without resuscitating the Score instance.
+@dataclass(slots = True)
+class _ScoreValues:
+    score: int
+    high_score: int
+
+    _BYTES_INT_CONVERSION_KWARGS = {'byteorder': 'big', 'signed': False}
+
+    def __init__(self):
+        self.score = 0
+        self.high_score = self._high_score_load()
+
+    def _high_score_load(self):
+        high_score = 0
+        if os.path.isfile(HIGH_SCORE_FILE):
+            with open(HIGH_SCORE_FILE, 'rb') as file:
+                high_score = int.from_bytes(file.read(HIGH_SCORE_FILE_NUM_BYTES), **self._BYTES_INT_CONVERSION_KWARGS)
+        return high_score
+
+    def _high_score_save(self):
+        print(f"Saving {self.high_score}")
+        with open(HIGH_SCORE_FILE, 'wb') as file:
+            file.write(self.high_score.to_bytes(length = HIGH_SCORE_FILE_NUM_BYTES, **self._BYTES_INT_CONVERSION_KWARGS))
+
+
 class Score:
     """Class Score. Class dealing with updating of the score."""
 
     def __init__(self):
         """Constructor for the class Score."""
-        self._score = 0
-        self._high_score = self._load_or_write_high_score(load = True)
+        self._scores = _ScoreValues()
+        
+        weakref.finalize(self, self._scores._high_score_save)
 
         self._ghost_eaten_same_fright = None
         
     
     # Defining properties for some private attributes.
-    score      = property(lambda self: self._score)
-    high_score = property(lambda self: self._high_score)
+    score      = property(lambda self: self._scores.score)
+    high_score = property(lambda self: self._scores.high_score)
     
 
     def __iadd__(self, value):
-        self._score += value
+        self._scores.score += value
 
-        if self._score > self._high_score:
-            self._high_score = self._score
-            self._load_or_write_high_score(load = False)
+        if self._scores.score > self._scores.high_score:
+            self._scores.high_score = self._scores.score
+            # Score will be written to disk by finalizer.
 
         return self
-
-    def _load_or_write_high_score(self, load):
-        
-        if load:
-            if not os.path.isfile(HIGH_SCORE_FILE):
-                return self._score
-
-            with open(HIGH_SCORE_FILE, 'rb') as file:
-                return int.from_bytes(file.read(HIGH_SCORE_FILE_NUM_BYTES), byteorder = 'big', signed = False)
-            
-        with open(HIGH_SCORE_FILE, 'wb') as file:
-            file.write(self._high_score.to_bytes(length = HIGH_SCORE_FILE_NUM_BYTES, byteorder = 'big', signed = False))
-
+    
     
     def add_to_score(self, action, level = None):
         """Adds points to the score based on the action type and data in parameters dictionary."""
